79d232a618cc67ca5580e7b286256dc0f8686cdd
LiordinoFilho <liordino.ubsdavila@gmail.com>
2026-08-07 17:56:05 -0300

    Delete mockData.ts

--- mockData.ts
+++
@@ -1,8 +0,0 @@
-import { GoogleContact, CalendarEvent, Domicile } from './types';
-
-export const INITIAL_CONTACTS: GoogleContact[] = [];
-
-export const INITIAL_DOMICILES: Domicile[] = [];
-
-export const getInitialEvents = (): CalendarEvent[] => [];
-
